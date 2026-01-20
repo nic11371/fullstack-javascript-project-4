1b7df81a297b6ad77c0e25ec396f8d3d4261a2c1
Николай <nikolay@MacBook-Air-Nikolaj.local>
2026-01-20 20:05:31 +0300

    lint

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,18 @@
-import js from '@eslint/js'
+import { createRequire } from 'module';
 import globals from 'globals'
 
+const require = createRequire(import.meta.url);
+const js = require('@eslint/js');
+
 export default [
-  js.configs.recommended,
-  {
-    files: ['**/*.{js,mjs,cjs}'],
-    languageOptions: {
-      globals: globals.node,
+    js.configs.recommended,
+    {
+        files: ['**/*.{js,mjs,cjs}'],
+        languageOptions: {
+            globals: {
+                ...globals.node,
+                ...globals.jest,
+            },
+        },
     },
-  },
 ]
