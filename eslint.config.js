614dc5f18a144f0bb0c3c4617c9a941cac322bfd
Николай <nikolay@MacBook-Air-Nikolaj.local>
2026-01-20 19:49:45 +0300

    lint

--- eslint.config.js
+++ eslint.config.js
@@ -1,21 +1,12 @@
-import { createRequire } from 'module';
-import globals from 'globals';
-
-const require = createRequire(import.meta.url);
-const pluginJs = require('@eslint/js');
-
-// Обработка разных вариантов экспорта (CJS/ESM interop)
-const jsConfig = pluginJs.configs || pluginJs.default.configs;
+import js from '@eslint/js'
+import globals from 'globals'
 
 export default [
-    jsConfig.recommended,
-    {
-        files: ['**/*.{js,mjs,cjs}'],
-        languageOptions: {
-            globals: {
-                ...globals.node,
-                ...globals.jest,
-            },
-        },
+  js.configs.recommended,
+  {
+    files: ['**/*.{js,mjs,cjs}'],
+    languageOptions: {
+      globals: globals.node,
     },
+  },
 ]
